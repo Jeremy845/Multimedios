5c69ce6352b7b21e563eeadb8e183b5bcfa0cd33
Jeremy845 <alvarezjeremy50@gmail.com>
2020-06-19 14:53:06 -0600

    Iniciando el back end

---
+++ PHP/Connection.php
@@ -0,0 +1,10 @@
+<?php
+$host="localhost";
+$user ="admin";
+$password="123";
+$db="yo_me_apunto_db";
+$connection= mysql_connect($host,$user,$password,$db);
+mysqli_set_charset($connection, "utf8");
+
+
+?>
