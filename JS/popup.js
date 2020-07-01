f7a72dc8f9b9d3ac1bbcfa97369852b061bb9f39
Jeremy845 <alvarezjeremy50@gmail.com>
2020-06-30 19:51:51 -0600

    Slideshow

--- JS/popup.js
+++ JS/popup.js
@@ -1,12 +1,50 @@
 var btnAbrirPopup = document.getElementById('btn-abrir-popup'),
+
 	overlay = document.getElementById('overlay'),
 	popup = document.getElementById('popup'),
-	btnCerrarPopup = document.getElementById('btn-cerrar-popup');
+    btnCerrarPopup = document.getElementById('btn-cerrar-popup');
+    //----------------------
+
+    var btnAbrirPopup1 = document.getElementById('btn-abrir-popup-1'),
+
+	overlay = document.getElementById('overlay'),
+	popup = document.getElementById('popup'),
+    btnCerrarPopup = document.getElementById('btn-cerrar-popup');
+
+    //------------------------
+    var btnAbrirPopup2 = document.getElementById('btn-abrir-popup-2'),
+
+	overlay = document.getElementById('overlay'),
+	popup = document.getElementById('popup'),
+    btnCerrarPopup = document.getElementById('btn-cerrar-popup');
+    //------------------------------------
+    var btnAbrirPopup3 = document.getElementById('btn-abrir-popup-3'),
+
+	overlay = document.getElementById('overlay'),
+	popup = document.getElementById('popup'),
+    btnCerrarPopup = document.getElementById('btn-cerrar-popup');
+    
 
 btnAbrirPopup.addEventListener('click', function(){
 	overlay.classList.add('active');
 	popup.classList.add('active');
 });
+//-----------------------------------------
+btnAbrirPopup1.addEventListener('click', function(){
+	overlay.classList.add('active');
+	popup.classList.add('active');
+});
+//------------------------------------
+btnAbrirPopup2.addEventListener('click', function(){
+	overlay.classList.add('active');
+	popup.classList.add('active');
+});
+//---------------------------------
+btnAbrirPopup3.addEventListener('click', function(){
+	overlay.classList.add('active');
+	popup.classList.add('active');
+});
+//--------------
 
 btnCerrarPopup.addEventListener('click', function(e){
 	e.preventDefault();
---
+++ JS/slideshow.js
@@ -0,0 +1,13 @@
+
+
+function slideshow(){
+   $('.autoplay').slick({
+     slidesToShow: 3,
+       slidesToScroll: 1,
+autoplay: true,
+autoplaySpeed: 2000,
+         });
+ };
+
+
+
